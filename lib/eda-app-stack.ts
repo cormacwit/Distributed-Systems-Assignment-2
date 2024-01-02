7cb4c1bfe19574cb8911fea9114c48f9c3cebbae
cormacwit <20093840@mail.wit.ie>
2024-01-02 19:38:40 +0000

    Completed Fan out implementation - added emailing logic!

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -1,80 +1,97 @@
-import * as cdk from "aws-cdk-lib";
-import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as s3n from "aws-cdk-lib/aws-s3-notifications";
-import * as events from "aws-cdk-lib/aws-lambda-event-sources";
-import * as sqs from "aws-cdk-lib/aws-sqs";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as iam from "aws-cdk-lib/aws-iam";
+import * as cdk from 'aws-cdk-lib';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as events from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
-import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
 
 export class EDAAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
-
-    const imagesBucket = new s3.Bucket(this, "images", {
+    const imagesBucket = new s3.Bucket(this, 'images', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
     });
 
-
-        // Integration infrastructure
-
-        const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
-          receiveMessageWaitTime: cdk.Duration.seconds(10),
-        });
-        const newImageTopic = new sns.Topic(this, "NewImageTopic", {
-          displayName: "New Image topic",
-        }); 
+    // Integration infrastructure
+    const imageProcessQueue = new sqs.Queue(this, 'img-created-queue', {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
     
-  // Lambda functions
+    const newImageTopic = new sns.Topic(this, 'NewImageTopic', {
+      displayName: 'New Image topic',
+    });
 
-  const processImageFn = new lambdanode.NodejsFunction(
-    this,
-    "ProcessImageFn",
-    {
-      // architecture: lambda.Architecture.ARM_64,
+    const mailerQ = new sqs.Queue(this, 'mailer-queue', {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
+    const mailerFn = new lambdanode.NodejsFunction(this, 'mailer-function', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
+    // Lambda functions
+    const processImageFn = new lambdanode.NodejsFunction(this, 'ProcessImageFn', {
       runtime: lambda.Runtime.NODEJS_18_X,
       entry: `${__dirname}/../lambdas/processImage.ts`,
       timeout: cdk.Duration.seconds(15),
       memorySize: 128,
-    }
-  );
+    });
 
-  // Event triggers
+    // Event triggers
+    imagesBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SnsDestination(newImageTopic)
+    );
 
-  imagesBucket.addEventNotification(
-    s3.EventType.OBJECT_CREATED,
-    new s3n.SnsDestination(newImageTopic)  // Changed
-);
+    const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    });
 
-  const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
-    batchSize: 5,
-    maxBatchingWindow: cdk.Duration.seconds(10),
-  });
+    processImageFn.addEventSource(newImageEventSource);
 
-  processImageFn.addEventSource(newImageEventSource);
+    // Permissions
+    imagesBucket.grantRead(processImageFn);
 
-  // Permissions
+    // Connect mailerQ as a subscriber to newImageTopic
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
 
-  imagesBucket.grantRead(processImageFn);
+    // Create a new event source from the mailerQ
+    const mailerEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    });
 
-  // Output
-  
-  new cdk.CfnOutput(this, "bucketName", {
-    value: imagesBucket.bucketName,
-  });
+    // Make the mailerFn trigger on messages from mailerQ
+    mailerFn.addEventSource(mailerEventSource);
+
+    // Give mailerFn permissions to send emails using SES
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'ses:SendEmail',
+          'ses:SendRawEmail',
+          'ses:SendTemplatedEmail',
+        ],
+        resources: ['*'],
+      })
+    );
 
     // Output
-    
-    new cdk.CfnOutput(this, "bucketName", {
+    new cdk.CfnOutput(this, 'bucketName', {
       value: imagesBucket.bucketName,
     });
   }
